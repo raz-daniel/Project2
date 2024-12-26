dce27d7dc28dd9b7d4e6cc7c32824da3dadfc030
raz-daniel <raz.daniel@gmail.com>
2024-12-27 00:04:44 +0200

    finish creating localStorage for moreInfo button with twoMinutes timestamp

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,87 @@
 
 (() => {
 
+    const collectCoinData = async coinApi => {
+        const response = await fetch(coinApi)
+        if (!response.ok) throw new Error(`Failed to receive data from server`)
+        const coin = await response.json()
+        return coin;
+    }
+
+    const generateCoinCollapse = coin => {
+        return `
+                            <div class="card card-body" style="width: 300px;">
+                                <img class="collapseImg" src="${coin.image.thumb}" alt="${coin.name}">
+                                <br>
+                                <p>Current Price (EUR): ${coin.market_data.current_price.eur} €</p>
+                                <p>Current Price (USD): ${coin.market_data.current_price.usd} $</p>
+                                <p>Current Price (ILS): ₪ ${coin.market_data.current_price.ils}</p>
+                            </div>
+                 `
+    }
+
+    const renderCoinCollapse = (coinHTML, coinId) => document.getElementById(coinId).innerHTML = coinHTML
+
+    const isCacheValid = timestamp => {
+        const now = Date.now();
+        const twoMinutes = 1000*60*2;
+        return (now - timestamp) < twoMinutes
+    }
+
+    const getCachedData = coinId => {
+        try {
+            const cached = localStorage.getItem(`crypto${coinId}`)
+            if (!cached) return null
+
+            const {data, timestamp} = JSON.parse(cached);
+
+            if (!isCacheValid(timestamp)) {
+                localStorage.removeItem(`crypto${coinId}`)
+                return null
+            }
+
+            return data;
+
+        } catch (error) {
+            console.warn('Error reading from cache:', error)
+            return null;
+        }
+    }
+
+    const  saveToCache = (coinId, data) => {
+        try {
+            const cacheData = {
+                data,
+                timestamp: Date.now()
+            }
+            localStorage.setItem(`crypto${coinId}`, JSON.stringify(cacheData) )
+        } catch (error) {
+            console.warn('Error Saving to catch', error)
+        }
+    }
+
+    const moreInfo = async coinId => {
+        try {
+            const cachedData = getCachedData(coinId);
+            if (cachedData) {
+                const coinCollapse = generateCoinCollapse(cachedData);
+                const coinIdCollapse = `collapse${coinId}`;    
+                renderCoinCollapse(coinCollapse, coinIdCollapse);
+                return;
+            }
+
+            const coinApi = `https://api.coingecko.com/api/v3/coins/${coinId}`
+            const coinData = await collectCoinData(coinApi)
+            const coinCollapse = generateCoinCollapse(coinData)
+            const coinIdCollapse = `collapse${coinId}`
+            renderCoinCollapse(coinCollapse, coinIdCollapse)
+            saveToCache(coinId, coinData)
+        } catch (error) {
+            console.warn(error)
+            document.getElementById('main').innerHTML = `<p>Error loading data</p>`;
+        }
+    }
+
     const collectData = async url => {
         const response = await fetch(url)
         if (!response.ok) throw new Error(`Failed to receive data from server`)
@@ -44,49 +125,6 @@
     }
 
     const renderCoinsHTML = coinsHTML => document.getElementById('coins').innerHTML = coinsHTML;
-
-    const collectCoinData = async coinApi => {
-        const response = await fetch(coinApi)
-        if (!response.ok) throw new Error(`Failed to receive data from server`)
-        const coin = await response.json()
-        return coin;
-    }
-
-    const generateCoinCollapse = coin => {
-        return `
-                            <div class="card card-body" style="width: 300px;">
-                                <img class="collapseImg" src="${coin.image.thumb}" alt="${coin.name}">
-                                <br>
-                                <p>Current Price (EUR): ${coin.market_data.current_price.eur} €</p>
-                                <p>Current Price (USD): ${coin.market_data.current_price.usd} $</p>
-                                <p>Current Price (ILS): ₪ ${coin.market_data.current_price.ils}</p>
-                            </div>
-                 `
-    }
-
-    const renderCoinCollapse = (coinHTML, coinId) => {
-        console.log("Rendering collapse for:", coinId);
-        const collapseElement = document.getElementById(coinId);
-        if (!collapseElement) {
-            console.warn(`Element with id ${coinId} not found in the DOM`)
-            return
-        }
-        collapseElement.innerHTML = coinHTML
-    }
-
-    const moreInfo = async coinID => {
-        try {
-            const coinApi = `https://api.coingecko.com/api/v3/coins/${coinID}`
-            const coinData = await collectCoinData(coinApi)
-            const coinCollapse = generateCoinCollapse(coinData)
-            const coinId = `collapse${coinData.id}`
-            renderCoinCollapse(coinCollapse, coinId)
-        } catch (error) {
-            console.warn(error)
-            document.getElementById('main').innerHTML = `<p>Error loading data</p>`;
-        }
-
-    }
 
     const addToMoreInfoEventListeners = () => {
         document.querySelectorAll('.btn-primary').forEach(button => {
